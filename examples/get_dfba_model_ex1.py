bef415ff72c638cb859b9b11e2c03be3e998a292
erikadudki <edudkin@uni-bonn.de>
2021-05-18 09:01:14 +0200

    tidy up

--- examples/get_dfba_model_ex1.py
+++ examples/get_dfba_model_ex1.py
@@ -10,21 +10,25 @@ from multiprocessing import Lock
 # )
 
 
-def get_dfba_model():
-    fba_model = read_sbml_model("/home/erika/Documents/Projects/DFBA/dynamic-fba/"
-                                "sbml-models/iJR904.xml.gz")
+def get_dfba_model(model_dir):
+    """Anaerobic growth of *E. coli* on glucose and xylose.
+
+    Organism -> _Escherichia coli str. K-12 substr. MG1655_
+    Model in http://bigg.ucsd.edu/models/iJR904
+    """
+    fba_model = read_sbml_model(model_dir)
 
     fba_model.solver = "glpk"
     dfba_model = DfbaModel(fba_model)
 
-    # instances of KineticVariable (default initial conditions are 0.0, but can be
-    # set here if wanted e.g. Oxygen)
+    # instances of KineticVariable (default initial conditions are 0.0, but
+    # can be set here if wanted e.g. Oxygen)
     X = KineticVariable("Biomass")
     Gluc = KineticVariable("Glucose")
     Xyl = KineticVariable("Xylose")
     Oxy = KineticVariable("Oxygen", initial_condition=0.24)
     Eth = KineticVariable("Ethanol")
-    #
+
     # add kinetic variables to dfba_model
     dfba_model.add_kinetic_variables([X, Gluc, Xyl, Oxy, Eth])
 
@@ -42,16 +46,15 @@ def get_dfba_model():
     params = {"K_g": 0.0027,
               "v_gmax": 10.5,
               "K_z": 0.0165,
-              "v_zmax": 6.0 }
-    K_g = Parameter(list(params.keys())[0],list(params.values())[0])
-    v_gmax = Parameter(list(params.keys())[1],list(params.values())[1])
+              "v_zmax": 6.0}
+    K_g = Parameter(list(params.keys())[0], list(params.values())[0])
+    v_gmax = Parameter(list(params.keys())[1], list(params.values())[1])
     K_z = Parameter(list(params.keys())[2], list(params.values())[2])
-    v_zmax = Parameter(list(params.keys())[3],list(params.values())[3])
+    v_zmax = Parameter(list(params.keys())[3], list(params.values())[3])
 
-    dfba_model.add_parameters([K_g,v_gmax,K_z,v_zmax])
+    dfba_model.add_parameters([K_g, v_gmax, K_z, v_zmax])
     # dfba_model.add_parameters([K_g,K_z])#,init_biomass,init_glucose,
                               # init_xylose,init_oxygen,init_ethanol])
-
 
     # add rhs expressions for kinetic variables in dfba_model
     dfba_model.add_rhs_expression("Biomass", mu * X)
@@ -60,24 +63,25 @@ def get_dfba_model():
     dfba_model.add_rhs_expression("Oxygen", v_O * 16.0 * X / 1000.0)
     dfba_model.add_rhs_expression("Ethanol", v_E * 46.06844 * X / 1000.0)
 
-    # add lower/upper bound expressions for exchange fluxes in dfba_model together
-    # with expression that must be non-negative for correct evaluation of bounds
+    # add lower/upper bound expressions for exchange fluxes in dfba_model
+    # together with expression that must be non-negative for correct evaluation
+    # of bounds
     # v_gmax = 10.5
     # K_g = 0.0027
     dfba_model.add_exchange_flux_lb(
-        "EX_glc(e)", v_gmax * (Gluc / (K_g + Gluc)) * (1 / (1 + Eth / 20.0)), Gluc
-    ) #v_g glucose
-    dfba_model.add_exchange_flux_lb("EX_o2(e)", 15.0 * (Oxy / (0.024 + Oxy)), Oxy) # v_o, oxygen
+        "EX_glc(e)", v_gmax * (Gluc / (K_g + Gluc)) * (1 / (1 + Eth / 20.0)),
+        Gluc)   # v_g glucose
+    dfba_model.add_exchange_flux_lb("EX_o2(e)", 15.0 * (Oxy / (0.024 + Oxy)),
+                                    Oxy)    # v_o, oxygen
     # v_zmax = 6.0
     # K_z = 0.0165
     dfba_model.add_exchange_flux_lb(
         "EX_xyl_D(e)",
-        v_zmax * (Xyl / (K_z + Xyl)) * (1 / (1 + Eth / 20.0)) * (1 / (1 + Gluc / 0.005)),
-        Xyl,
-    ) # v_z, xylose
+        v_zmax * (Xyl / (K_z + Xyl)) * (1 / (1 + Eth / 20.0)) *
+        (1 / (1 + Gluc / 0.005)), Xyl)  # v_z, xylose
 
-    # add initial conditions for kinetic variables in dfba_model biomass (gDW/L),
-    # metabolites (g/L)
+    # add initial conditions for kinetic variables in dfba_model biomass
+    # (gDW/L), metabolites (g/L)
     dfba_model.add_initial_conditions(
         {
             "Biomass": 0.03,
@@ -87,21 +91,6 @@ def get_dfba_model():
             "Ethanol": 0.0,
         }
     )
-    # "Biomass": 7.09720,
-    # "Glucose": 16.828,
-    # "Xylose": 0,
-    # "Oxygen": 0.0,
-    # "Ethanol": 0.0,
-    # "Biomass": 0.03,
-    # "Glucose": 15.5,
-    # "Xylose": 8.0,
-    # "Oxygen": 0.0,
-    # "Ethanol": 0.0,
-    # "Biomass": 0.1,
-    # "Glucose": 16,
-    # "Xylose": 8.0,
-    # "Oxygen": 0.0,
-    # "Ethanol": 0.0,
 
     return dfba_model, params
 
@@ -111,8 +100,8 @@ def modifun(dfba_model):
 
     Everything that's needed after setup should go in here.
     """
-    # instances of KineticVariable (default initial conditions are 0.0, but can be
-    # set here if wanted e.g. Oxygen)
+    # instances of KineticVariable (default initial conditions are 0.0, but
+    # can be set here if wanted e.g. Oxygen)
     X = KineticVariable("Biomass")
     Gluc = KineticVariable("Glucose")
     Xyl = KineticVariable("Xylose")
@@ -122,8 +111,6 @@ def modifun(dfba_model):
     # add kinetic variables to dfba_model
     dfba_model.add_kinetic_variables([X, Gluc, Xyl, Oxy, Eth])
 
-    # ...
-    #
     # instances of ExchangeFlux
     mu = ExchangeFlux("BiomassEcoli")
     v_G = ExchangeFlux("EX_glc(e)")
@@ -155,8 +142,9 @@ def modifun(dfba_model):
     dfba_model.add_rhs_expression("Oxygen", v_O * 16.0 * X / 1000.0)
     dfba_model.add_rhs_expression("Ethanol", v_E * 46.06844 * X / 1000.0)
 
-    # add lower/upper bound expressions for exchange fluxes in dfba_model together
-    # with expression that must be non-negative for correct evaluation of bounds
+    # add lower/upper bound expressions for exchange fluxes in dfba_model
+    # together with expression that must be non-negative for correct evaluation
+    # of bounds
     # v_gmax = 10.5
     # K_g = 0.0027
     dfba_model.add_exchange_flux_lb(
@@ -168,10 +156,10 @@ def modifun(dfba_model):
     dfba_model.add_exchange_flux_lb(
         "EX_xyl_D(e)",
         v_zmax * (Xyl / (K_z + Xyl)) * (1 / (1 + Eth / 20.0)) * (
-                    1 / (1 + Gluc / 0.005)),Xyl)  # v_z, xylose
+                    1 / (1 + Gluc / 0.005)), Xyl)  # v_z, xylose
 
-    # add initial conditions for kinetic variables in dfba_model biomass (gDW/L),
-    # metabolites (g/L)
+    # add initial conditions for kinetic variables in dfba_model biomass
+    # (gDW/L), metabolites (g/L)
     dfba_model.add_initial_conditions(
         {
             "Biomass": 0.03,
@@ -181,6 +169,7 @@ def modifun(dfba_model):
             "Ethanol": 0.0,
         }
     )
+    dfba_model.solver_data.set_display("none")
 
 
 class PicklableDFBAModel:
@@ -203,7 +192,6 @@ class PicklableDFBAModel:
 
     def simulate(self, t_start, t_end, t_out, *args, **kwargs):
         """Simulate from the model."""
-        self.dfba_model.solver_data.set_display("none")
         concentrations, trajectories = self.dfba_model.simulate(t_start,
                                                                 t_end, t_out)
         # return self.dfba_model(*args, **kwargs)
@@ -212,7 +200,6 @@ class PicklableDFBAModel:
     def __getstate__(self) -> dict:
         """Get a serializable representation of the object for pickling."""
         return {
-            # print("I was actually called")
             'file_': self.file_,
             'modifun': self.modifun
             # 'lock': self.lock
@@ -225,6 +212,7 @@ class PicklableDFBAModel:
         with multiprocessing.Lock():
             self.dfba_model = PicklableDFBAModel.create_dfba_model(
                 self.file_, self.modifun)
+            # self.dfba_model.solver_data.set_display("none")
 
     @staticmethod
     def create_dfba_model(file_, modifun):
@@ -235,9 +223,6 @@ class PicklableDFBAModel:
         return dfba_model
 
 
-
-#erikas_dfba_model = PicklableDFBAModel("/home/erika/Documents/Projects/DFBA/dynamic-fba/sbml-models/iJR904.xml.gz", modifun)
-
 # import cloudpickle as pickle
 # dump = pickle.dumps(obj);
-# pickle.loads(dump) #drauf aufrufen, mit
+# pickle.loads(dump)
